d320c35c77d162a3b6a4eb111f5d3c78a094ae5c
manjeetverma <manjeetverma@gmail.com>
2023-01-19 21:17:12 +0530

    made error-class and utiliti function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path=require('path')
 const campground = require('./models/campground')
 const methodoverride=require('method-override')
 const ejsMate=require('ejs-mate')
+const wrapAsync=require('./utiliti/wrapAsync')
 
 app.use(methodoverride('_method'))
 app.use(express.urlencoded({extended:true}))
@@ -20,72 +21,55 @@ mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true
         console.log("Error, MONGO CONNECTION!!!!")
         console.log(err)
     })
-app.get('/camps',async(req,res,next)=>{
-    try{
+app.get('/camps',wrapAsync(async(req,res)=>{
         const camp=await campground.find({});
         res.render('campgrounds/index',{camp})
-    }catch(e){
-        next(e)
-    }
+   
     
-})
+}))
 app.get('/camps/new',(req,res)=>{
     res.render('campgrounds/new')
 })
-app.post('/camps',async(req,res,next)=>{
-    try{
+app.post('/camps',wrapAsync(async(req,res)=>{
         const c=new campground(req.body.campground)
         await c.save()
         res.redirect(`camps/${c._id}`)
-    }catch(err){
-        next(err)
-    }
+   
 
-})
-app.get('/camps/:id',async(req,res,next)=>{
-  try{
+}))
+app.get('/camps/:id',wrapAsync(async(req,res)=>{
     const camp=await campground.findById(req.params.id);
     res.render('campgrounds/detail',{camp})
-}catch(e){
-    next(e)
-}
-})
 
-app.get('/camps/:id/edit',async(req,res,next)=>{
-    try{
+}))
+
+app.get('/camps/:id/edit',wrapAsync(async(req,res)=>{
     const camp=await campground.findById(req.params.id);
     res.render('campgrounds/edit',{camp})
-}catch(e){
-    next(e)
-}
-})
-app.put('/camps/:id',async(req,res,next)=>{
-    try{
+
+}))
+app.put('/camps/:id',wrapAsync(async(req,res)=>{
     const {id}=req.params
     const c=await campground.findByIdAndUpdate(id,{...req.body.campground})//spread opr used for object to be expanded in places where zero or more key-value pairs (for object literals) are expected.
     res.redirect(`/camps/${c._id}`)
-}catch(e){
-    next(e)
-}
 
-})
-app.delete('/camps/:id',async(req,res,next)=>{
-    try{
+
+}))
+app.delete('/camps/:id',wrapAsync(async(req,res)=>{
     const {id}=req.params;
     await campground.findByIdAndDelete(id);
     res.redirect('/camps')
-}catch(e){
-    next(e)
-}
-})
+
+}))
 
 
 
 app.get('/',async (req,res)=>{
     res.send('Home Page')
 })
+
 app.use((err,req,res,next)=>{
-    res.send('something went wrong!')
+    res.send('ooh my god, something went wrong!')
 })
 
 app.listen('3000',(req,res)=>{
---
+++ utiliti/expressError.js
@@ -0,0 +1,8 @@
+class expressError extends Error{
+    constructor(message,statuscode){
+        super();
+        this.message=message
+        this.statuscode=statuscode
+    }
+}
+module.exports=expressError;
