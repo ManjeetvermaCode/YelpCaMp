7fc4d2f4658134e3482f0e820e6108644c4d873d
manjeetverma <manjeetverma@gmail.com>
2023-01-23 20:40:25 +0530

    added server side validation to post route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const campground = require('./models/campground')
 const methodoverride=require('method-override')
 const ejsMate=require('ejs-mate')
 const wrapAsync=require('./utiliti/wrapAsync')
+const expressError = require('./utiliti/expressError')
+const { stat } = require('fs')
+const joi=require('joi')
+const { threadId } = require('worker_threads')
+const { title } = require('process')
 
 app.use(methodoverride('_method'))
 app.use(express.urlencoded({extended:true}))
@@ -31,10 +36,27 @@ app.get('/camps/new',(req,res)=>{
     res.render('campgrounds/new')
 })
 app.post('/camps',wrapAsync(async(req,res)=>{
+    //if(!req.body.campground) throw new expressError('invalid camp data',400)
+    const campgroundSchema=joi.object({
+    campground:joi.object({
+        title:joi.string().required(),
+        price:joi.number().required().min(0),
+        image:joi.string().required(),
+        description:joi.string().required(),
+        location:joi.string().required()
+     } )
+    }).required()
+    const {error}=campgroundSchema.validate(req.body)
+    if(error){
+        const msg=error.details.map(e=>e.message).join(',')
+        throw new expressError(msg,400)
+    }
+    console.log(error)
         const c=new campground(req.body.campground)
         await c.save()
         res.redirect(`camps/${c._id}`)
-   
+    
+        
 
 }))
 app.get('/camps/:id',wrapAsync(async(req,res)=>{
@@ -68,8 +90,14 @@ app.get('/',async (req,res)=>{
     res.send('Home Page')
 })
 
+app.all('*',(req,res,next)=>{
+    next(new expressError('invalid route',404))//will get call if not route matches, allows us to customize error msg and status code.
+})
+
 app.use((err,req,res,next)=>{
-    res.send('ooh my god, something went wrong!')
+    const {statuscode=500}=err
+    if(!err.message) err.message='someting went wrong'
+    res.status(statuscode).render('error',{err})
 })
 
 app.listen('3000',(req,res)=>{
