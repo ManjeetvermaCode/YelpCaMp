7ddff589d6bb33c9cfc86ea6e688cd38bef8d413
manjeetverma <manjeetverma@gmail.com>
2023-02-07 18:14:34 +0530

    added a form in detail page for sending review on campground

--- utiliti/expressError.js
+++ utiliti/expressError.js
@@ -1,6 +1,6 @@
 class expressError extends Error{
     constructor(message,statuscode){
-        super();
+        super();//this will inherit all the parents methods and value to child class.
         this.message=message
         this.statuscode=statuscode
     }
