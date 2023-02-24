3fd672c977bd820fbfec0dd5f05e951386b2b4a1
manjeetverma <manjeetverma@gmail.com>
2023-02-24 21:27:48 +0530

    Alert message will be display whenever a user will add and delete review

--- routes/reviewroutes.js
+++ routes/reviewroutes.js
@@ -29,6 +29,7 @@ router.post('/',validatereview,wrapAsync(async(req,res)=>{
     cg.review.push(r)
    await cg.save()
    await r.save()
+   req.flash('success',"New review has been added!!")
    res.redirect(`/camps/${id}`)
 }))
 
@@ -37,6 +38,8 @@ router.delete('/:reviewid',wrapAsync(async(req,res)=>{
     
     await review.findByIdAndDelete(reviewid);//1.here we are only deleting review individually,
     await campground.findByIdAndUpdate(id,{$pull:{review:reviewid}})//but we also want to delete [findByIdAndUpdate] the reference to review in array, which can couse problem in a big production app. 
+    req.flash('success',"New review has been deleted!!")
+
     res.redirect(`/camps/${id}`)
 
 }))
