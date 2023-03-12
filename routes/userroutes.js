47178900b9c6444cbe167bdbe4913b87e4a87c36
manjeetverma <manjeetverma@gmail.com>
2023-03-12 20:10:19 +0530

    wrote login route and defined 'passport.authenticate' middleware which will handle authontication

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const passport = require('passport')
 const router=express.Router()
 const user=require('../models//user')
 const Wrapasync=require('../utiliti/wrapAsync')
@@ -10,8 +11,8 @@ router.get('/register',(req,res)=>{
 router.post('/register',Wrapasync(async(req,res)=>{
     try{
         const {username,email,password}=req.body
-        const registeruser=new user({username,email})
-       const result= await user.register(registeruser,password)
+        const registeruser=new user({username,email})//creating new user model
+       const result= await user.register(registeruser,password)//register method will create salt and hash the password for us.`
         console.log(result)
         req.flash('success','Welcome to YelpCamp!')
         res.redirect('/camps')
@@ -19,8 +20,15 @@ router.post('/register',Wrapasync(async(req,res)=>{
         req.flash('error',e.message)
         res.redirect('/register')
     }
-    
-    
 }))
+
+router.get('/login',(req,res)=>{
+    res.render('users/login')
+})
+
+router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{//passport.authenticate is an middleware, it will authenticate automatically and will handler if error occur else it will move 'next';
+    req.flash('success',"You are Welcome")
+    res.redirect('/camps')
+})
 
 module.exports=router
