206220c2a4be9c7378f58b69130b195ba4979883
manjeetverma <manjeetverma@gmail.com>
2023-03-13 12:05:41 +0530

    up untill now if a new user registered he'll have to login again to access full features, which is annoying so, we'll use 'req.login' which will log a user in after he registered.

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 
 
 module.exports.isLoggedIn=(req,res,next)=>{
-    console.log("Req.user :" ,req.user)
     if(!req.isAuthenticated()){//this method is provided by passport
         req.flash('error','You must signed in!!!')
        return res.redirect('/login')//return is needed other wise we'll get error ( Cannot set headers after they are sent to the client)
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -8,13 +8,17 @@ router.get('/register',(req,res)=>{
     res.render('users/register')
 })
 
-router.post('/register',Wrapasync(async(req,res)=>{
+router.post('/register',Wrapasync(async(req,res,next)=>{
     try{
         const {username,email,password}=req.body
         const registeruser=new user({username,email})//creating new user model
-       const result= await user.register(registeruser,password)//register method will create salt and hash the password for us.`
+       const result= await user.register(registeruser,password)//register method will create salt and hash the password for us
+       req.login(result,(err)=>{//up untill now if a new user registered he'll have to login again to access full features, which is annoying so, we'll use 'req.login' which will log a user in after he registered.
+        if(err) return next()
         req.flash('success','Welcome to YelpCamp!')
         res.redirect('/camps')
+       })
+        
     }catch(e){
         req.flash('error',e.message)
         res.redirect('/register')
