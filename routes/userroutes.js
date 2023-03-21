362d1548bb0381d21789e2b9575761cd7e6a0b5f
manjeetverma <manjeetverma@gmail.com>
2023-03-21 11:24:03 +0530

    we shorted out routes even more by nesting routes with same prefix and name together using router.route

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,14 +3,16 @@ const passport = require('passport')
 const router=express.Router()
 const wrapAsync = require('../utiliti/wrapAsync')
 const user=require('../controller/user')
+const { route } = require('./campgroundroutes')
 
-router.get('/register',user.renderRegisterForm)
+router.route('/register')
+    .get(user.renderRegisterForm)
+    .post(wrapAsync(user.CreateNewUser))
 
-router.post('/register',wrapAsync(user.CreateNewUser))
+router.route('/login')
+    .get(user.renderLoginForm)   
+    .post(passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),user.login)//passport.authenticate is an middleware, it will authenticate automatically and will handler if error occur else it will move 'next';
 
-router.get('/login',user.renderLoginForm)
-
-router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),user.login)//passport.authenticate is an middleware, it will authenticate automatically and will handler if error occur else it will move 'next';
 
 router.get("/logout", user.logout);
   
