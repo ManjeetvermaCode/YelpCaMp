b6b2bab44dc9f456bd870111afcc9d83979f2425
manjeetverma <manjeetverma@gmail.com>
2023-03-23 19:05:51 +0530

    modified new.ejs's form and installed and setup multer

--- routes/campgroundroutes.js
+++ routes/campgroundroutes.js
@@ -3,10 +3,15 @@ const router=express.Router()
 const wrapAsync=require('../utiliti/wrapAsync')
 const campground2=require('../controller/campground')
 const {isLoggedIn,isauthor,validatecampground}=require('../middleware')
+const multer=require('multer')
+const upload=multer({desc:'uploads/'})
 
 router.route('/')
     .get(wrapAsync(campground2.index))
-    .post(validatecampground,isLoggedIn,wrapAsync(campground2.CreateNewCamp))
+    //.post(validatecampground,isLoggedIn,wrapAsync(campground2.CreateNewCamp))
+    .post(upload.array('image',2),(req,res)=>{
+        console.log(req.body,req.files)
+    })
 
 router.get('/new',isLoggedIn,campground2.RenderNewForm)
 
